b7eb50334ea0a54babc9d7f565da300efb698617
Deepak Prajapat <94034668+deepak411prajapat@users.noreply.github.com>
2025-11-19 11:19:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('nav ul');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+}
